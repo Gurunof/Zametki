ce896013bf4782d47ed764fca2129237605db3c7
Gurunof <gurunof@gmail.com>
2015-01-28 22:58:03 +0300

    add date.h

---
+++ Zametki/date.h
@@ -0,0 +1,14 @@
+//
+//  date.h
+//  Zametki
+//
+//  Created by Dmitry on 1/28/15.
+//  Copyright (c) 2015 Dmitry. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface date : NSObject
+@property (nonatomic) NSDate *dateNow;
+
+@end
---
+++ Zametki/date.m
@@ -0,0 +1,17 @@
+//
+//  date.m
+//  Zametki
+//
+//  Created by Dmitry on 1/28/15.
+//  Copyright (c) 2015 Dmitry. All rights reserved.
+//
+
+#import "date.h"
+
+@implementation date
+
+-(void) datecount{
+    _dateNow = [NSDate date];
+}
+
+@end
